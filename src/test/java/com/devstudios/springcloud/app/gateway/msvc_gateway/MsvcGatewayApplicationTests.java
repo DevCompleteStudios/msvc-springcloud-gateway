3ff3c24a344d53b5832c5530606e501efeb03734
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-25 16:24:24 -0600

    First commit

---
+++ src/test/java/com/devstudios/springcloud/app/gateway/msvc_gateway/MsvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.springcloud.app.gateway.msvc_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
